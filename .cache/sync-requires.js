b8e2d59e2c8cb139be4e05f801f12d6c9a621653
Kaso <mackovision@gmail.com>
2024-02-08 09:58:30 -0800

    Bumping dependencies

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\macko\\OneDrive\\Documents\\GitHub\\hot_or_not_tub\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\macko\\OneDrive\\Documents\\GitHub\\hot_or_not_tub\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\macko\\OneDrive\\Documents\\GitHub\\hot_or_not_tub\\src\\pages\\index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("C:\\Users\\macko\\OneDrive\\Documents\\GitHub\\hot_or_not_tub\\src\\pages\\page-2.js")))
+}
+
